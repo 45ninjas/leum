575892e1f807ce3354b08d97008080bd9c276275
Those45Ninjas <tom.parker1235@gmail.com>
2018-09-24 20:15:01 +0800

    Updated the 'sensitive' config files.

--- leum/conf/database.conf.php
+++ leum/conf/database.conf.php
@@ -1,6 +1,19 @@
 <?php
+// The host-name/address of the database server. This is usually the 'local'
+// server.
 define('DB_HOST', 'localhost');
+
+// The database name. Change this if you are running multiple instances of
+// leum on the same database server.
+
+define('DB_NAME', 'leum');
+
+// The user-name and password of a user with the correct permission to edit
+// the database.
 define('DB_PASS', 'kYhNoDh0ookLjIMa');
 define('DB_USER', 'leum');
-define('DB_NAME', 'leum');
+
+
+// This file is included in the .gitignore file.
+// Revision 1
 ?>
--- leum/plugins/omdb_source/omdb_config.conf.php
+++ leum/plugins/omdb_source/omdb_config.conf.php
@@ -1,9 +1,13 @@
 <?php
 
+// Your Open Movie DataBase (omdb) api key. (shhh, it's a secret)
+define('OMDB_API_KEY', NULL);
+
 // Replace the value with your The Open Movie Database api key from
 // http://www.omdbapi.com/apikey.aspx.
 // If the OMDB_API_KEY is NULL the OMDB plug-in will automatically
 // disable itself.
-define('OMDB_API_KEY', NULL);
 
+// This file is included in the .gitignore file.
+// Revision 1
 ?>
